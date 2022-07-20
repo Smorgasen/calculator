7cd0907fb383fa60a2f2ed3fc89d49e10da5b96a
Smorgasen <yaroslaw.ivanko@gmail.com>
2022-07-20 17:53:55 +0300

    Start adding Event listeners

--- script.js
+++ script.js
@@ -32,13 +32,30 @@ function operate(operator, a, b) {
     }
 }
 
-
-
 const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operation]');
+const operatorButtons = document.querySelectorAll("[data-operation]");
 const equalsButton = document.getElementById('equalsBtn');
-const allClearButton = document.getElementById('clearBtn');
+const clearButton = document.getElementById('clearBtn');
 const deleteButton = document.getElementById('deleteBtn');
 const pointButton = document.getElementById('pointBtn');
-const lastOperationScreen = document.getElementById('lastOperatidataonScren');
+const lastOperationScreen = document.getElementById('lastOperationScren');
 const currentOperationScreen = document.getElementById('currentOperationScreen');
+
+let previousOperand = '';
+let currentOperand = '';
+let currentOperation = null;
+let screenReset = false;
+
+numberButtons.forEach(button =>
+    button.addEventListener('click', () => appendNumber(button.textContent))
+);
+operatorButtons.forEach(button =>
+    button.addEventListener('click', () => setOperator(button.textContent))
+);
+equalsButton.addEventListener('click', evaluate);
+clearButton.addEventListener('click', clear);
+deleteButton.addEventListener('click', deleteNumer);
+pointButton.addEventListener('click', appendPoint);
+
+
+
