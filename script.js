7eb68a2a0941e5c52a510aa6acd470cefff6ff96
Smorgasen <yaroslaw.ivanko@gmail.com>
2022-07-20 14:29:47 +0300

    Add basic calculator functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+// console.log (add(1, 3))
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+// console.log(subtract(1, 5))
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+// console.log (multiply(4, 2))
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+// console.log (divide(6, 2))
